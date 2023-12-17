4b551d7434f94e7e2c6685b194be357c5ceee531
svetozarzvkvc <svetozar.zivkovic13@gmail.com>
2023-12-17 21:42:43 +0100

    Added EditNote method
    Allowing users to edit their desired note.

--- NoteTakingApp/Program.cs
+++ NoteTakingApp/Program.cs
@@ -32,15 +32,18 @@ namespace NoteTakingApp
                 case "new":
                     CreateNewNote();
                     break;
+                case "edit":
+                    EditNote();
+                    break;
             }
         }
 
         private static void CreateNewNote()
         {
-            Console.Write("Enter name of note file, please?");
+            Console.WriteLine("Enter name of note file, please?");
             string noteNameInput = Console.ReadLine();
 
-            Console.Write("Enter note you want to save");
+            Console.WriteLine("Enter note you want to save");
             string noteBody = Console.ReadLine();
 
             XmlWriterSettings xmlFileSettings = new XmlWriterSettings();
@@ -49,9 +52,10 @@ namespace NoteTakingApp
             xmlFileSettings.CloseOutput = true;
             xmlFileSettings.OmitXmlDeclaration = true;
 
-            string fileName = $"{noteNameInput}{DateTime.Now.ToString("dd-MM-yy")}.xml";
+            //string fileName = $"{noteNameInput}{DateTime.Now.ToString("dd-MM-yy")}.xml";
+            string fileName = $"{noteNameInput}.xml";
 
-            using(XmlWriter newFile = XmlWriter.Create(NoteDirectoryPath + fileName, xmlFileSettings))
+            using (XmlWriter newFile = XmlWriter.Create(NoteDirectoryPath + fileName, xmlFileSettings))
             {
                 newFile.WriteStartDocument();
                 newFile.WriteStartElement("note");
@@ -62,6 +66,43 @@ namespace NoteTakingApp
                 newFile.Close();
             }
             Environment.Exit(0);
+        }
+
+        private static void EditNote()
+        {
+            string[] files = Directory.GetFiles(NoteDirectoryPath);
+            foreach (string file in files)
+            {
+                Console.WriteLine(Path.GetFileNameWithoutExtension(file));
+            }
+            Console.WriteLine("Enter the name of file you want to edit.");
+            string input = Console.ReadLine();
+            if (File.Exists(NoteDirectoryPath + input + ".xml"))
+            {
+
+                XmlDocument xmldoc = new XmlDocument();
+                try
+                {
+                    xmldoc.Load(NoteDirectoryPath + input + ".xml");
+                    var tekstfajla = xmldoc.SelectSingleNode("//noteBody").InnerText;
+                    Console.WriteLine(tekstfajla);
+
+                    string editedString = Console.ReadLine();
+                    xmldoc.SelectSingleNode("//noteBody").InnerText = editedString;
+                    xmldoc.Save(NoteDirectoryPath + input + ".xml");
+
+                    Environment.Exit(0);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Couldn't edit the note because of the following error: {e.Message}");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("File not found!\n");
+            }
         }
     }
 }
